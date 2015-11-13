90c485a34a06685f844b7f694f29de84cb9ccc8f
luin <i@zihua.li>
2015-11-13 23:00:45 +0800

    Finish tinsert command

--- lua/tinsert.lua
+++ lua/tinsert.lua
@@ -1,36 +1,62 @@
 local data = ARGV[argIndex + 1]
+
+-- Among BEFORE, AFTER and INDEX
 local insertType = string.upper(ARGV[argIndex + 2])
 local insertPivot = ARGV[argIndex + 3]
 
+local list = redis.call('get', key)
+if list then
+  list = cmsgpack.unpack(list)
+else
+  list = {}
+end
+
+-- Convert BEFORE and AFTER to INDEX by finding out
+-- the index of the pivot
 if insertType == 'BEFORE' or insertType == 'AFTER' then
-  local result = redis.call('linsert', key, insertType, insertPivot)
-  if result ~= -1 then
-    return result
+  for i, v in ipairs(items) do
+    if v == insertPivot then
+      insertPivot = i
+      break
+    end
   end
-  -- Pivot not found, fallback to INDEX mode
+
+  -- If pivot is not found, set the index to
+  -- head (BEFORE) or tail (AFTER)
+  if not index then
+    if insertType == 'BEFORE' then
+      insertPivot = 0
+    else
+      insertPivot = -1
+    end
+  end
+
   insertType = 'INDEX'
-  if insertType == 'BEFORE' then
-    insertPivot = '0'
-  else
-    insertPivot = '-1'
-  end
 end
 
-if insertType == 'INDEX' then
-  -- Fast way to insert item to head and tail
-  if insertPivot == '0' then
-    return redis.call('lpush', key, data)
-  elseif insertPivot == '-1' then
-    return redis.call('rpush', key, data)
-  end
-
-  local pivot = redis.call('lindex', key, insertPivot)
-  if pivot then
-    return redis.call('linsert', key, 'BEFORE', pivot, data)
-  end
-
-  -- Index is out of range, insert to tail
-  return redis.call('rpush', key, data)
+if insertType ~= 'INDEX' then
+  return redis.error_reply('ERR Invalid insert option')
 end
 
-return redis.error_reply('ERR Wrong insert option')
+local listLength = #list
+
+-- Support negative index
+if insertPivot < 0 then
+  insertPivot = listLength + insertPivot + 2
+else
+  insertPivot = insertPivot + 1
+end
+
+-- Handle out of range
+if insertPivot < 1 then
+  insertPivot = 1
+elseif insertPivot > listLength + 1 then
+  insertPivot = listLength + 1
+end
+
+table.insert(list, insertPivot, { data, 0 })
+
+redis.call('set', key, cmsgpack.pack(list))
+
+-- Return the inserted position
+return insertPivot
