6a0f0a231c9bffa0bb5f77f29afe04d6b819a3f5
luin <i@zihua.li>
2015-11-17 15:15:46 +0800

    Fix tparents

--- test/tparent.js
+++ test/tparent.js
@@ -1,29 +1,18 @@
-describe('tparent', function () {
-  beforeEach(function () {
-    return Promise.all([
-      redis.tinsert('tree', 'ROOT', '1'),
-      redis.tinsert('tree', 'ROOT', '2'),
-      redis.tinsert('tree', 'ROOT', '3'),
-      redis.tinsert('tree', '2', '4'),
-      redis.tinsert('tree', '4', '5'),
-      redis.tinsert('tree', '3', '6'),
-    ]);
-  });
-
-  it('returns nil when tree or node does not exist', function () {
-    return redis.tparent('non-exist tree', 'ROOT').then(function (res) {
-      expect(res).to.eql(null);
-      return redis.tparent('tree', 'non-exist node').then(function (res) {
-        expect(res).to.eql(null);
+describe('tparents', function () {
+  it('returns empty array when tree or node does not exist', function () {
+    return redis.tparents('non-exist tree', 'ROOT').then(function (res) {
+      expect(res).to.eql([]);
+      return redis.tparents('tree', 'non-exist node').then(function (res) {
+        expect(res).to.eql([]);
       });
     });
   });
 
   it('returns parent', function () {
-    return redis.tparent('tree', '6').then(function (res) {
-      expect(res).to.eql('3');
-      return redis.tparent('tree', '2').then(function (res) {
-        expect(res).to.eql('ROOT');
+    return redis.tparents('tree', '6').then(function (res) {
+      expect(res).to.eql(['3', '8']);
+      return redis.tparents('tree', '2').then(function (res) {
+        expect(res).to.eql(['ROOT1']);
       });
     });
   });
